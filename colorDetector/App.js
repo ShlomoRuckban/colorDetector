c6eb46eae4ab5afd1503e1f7a5a9e6bea68ceca1
ShlomoRuckban <Shlomoruckban@gmail.com>
2022-06-26 22:29:08 +0300

    fixed bugs

--- colorDetector/App.js
+++ colorDetector/App.js
@@ -1,12 +1,22 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState, useMemo} from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import ColorTile from './components/ColorTile';
+import {useSharedValue} from 'react-native-reanimated';
 import {
   Camera,
   useCameraDevices,
   useFrameProcessor,
+  onFrameProcessorPerformanceSuggestionAvailable,
 } from 'react-native-vision-camera';
+const DEFAULT_COLOR = '#000000';
 
 const App = () => {
+  const [frameProcessorFps, setFrameProcessorFps] = useState(3);
+  const firstColor = useSharedValue(DEFAULT_COLOR);
+  const secondColor = useSharedValue(DEFAULT_COLOR);
+  const thirdColor = useSharedValue(DEFAULT_COLOR);
+  const fourthColor = useSharedValue(DEFAULT_COLOR);
+  const fifthColor = useSharedValue(DEFAULT_COLOR);
   const devices = useCameraDevices('wide-angle-camera');
   const device = devices.back;
 
@@ -17,16 +27,61 @@ const App = () => {
     console.log(newCameraPermission, cameraPermission);
   };
 
+  const colorAnimationDuration = useMemo(
+    () => (1 / frameProcessorFps) * 1000,
+    [frameProcessorFps],
+  );
+
   const frameProcessor = useFrameProcessor(frame => {
     'worklet';
-    console.log(frame);
-    const test = __getColorPalette(frame)
-    console.log(test);
+    var array = __getColorPalette(frame);
+    var colors = [];
+    while (array.length > 0) {
+      colors.push({
+        pop: array.substring(
+          array.search(/population/i) + 12,
+          array.search(/population/i) +
+            12 +
+            array.slice(array.search(/population/i) + 12).search(']'),
+        ),
+        rgb: array.substring(
+          array.search(/rgb/i) + 5,
+          array.search(/rgb/i) +
+            5 +
+            array.slice(array.search(/rgb/i) + 5).search(']'),
+        ),
+      });
+      array = array.slice(
+        array.search(/population/i) +
+          12 +
+          array.slice(array.search(/population/i) + 12).search(']'),
+      );
+    }
+
+    colors = colors.filter(a => {
+      if ((a.rgb.length = 9)) return a;
+    });
+
+    colors = colors
+      .sort((a, b) => {
+        return b.pop - a.pop;
+      })
+      .slice(0, 5);
+      
+    if (colors.length < 5) return;
+    firstColor.value = colors[0].rgb;
+    secondColor.value =
+      colors[1].rgb === undefined ? DEFAULT_COLOR : colors[1].rgb;
+    thirdColor.value =
+      colors[2].rgb === undefined ? DEFAULT_COLOR : colors[2].rgb;
+    fourthColor.value =
+      colors[3].rgb === undefined ? DEFAULT_COLOR : colors[3].rgb;
+    fifthColor.value =
+      colors[4].rgb === undefined ? DEFAULT_COLOR : colors[4].rgb;
   }, []);
 
   useEffect(() => {
     getPermissions();
-    // console.log(firstColor)
   }, []);
 
   if (device == null)
@@ -43,9 +98,38 @@ const App = () => {
           device={device}
           isActive={true}
           frameProcessor={frameProcessor}
-          frameProcessorFps={1}
+          frameProcessorFps={3}
+          onFrameProcessorPerformanceSuggestionAvailable={
+            onFrameProcessorPerformanceSuggestionAvailable
+          }
         />
-        <View style={styles.infoContainer}></View>
+        <View style={styles.infoContainer}>
+          <ColorTile
+            name="1st"
+            color={firstColor}
+            animationDuration={colorAnimationDuration}
+          />
+          <ColorTile
+            name="2nd"
+            color={secondColor}
+            animationDuration={colorAnimationDuration}
+          />
+          <ColorTile
+            name="3rd"
+            color={thirdColor}
+            animationDuration={colorAnimationDuration}
+          />
+          <ColorTile
+            name="4th"
+            color={fourthColor}
+            animationDuration={colorAnimationDuration}
+          />
+          <ColorTile
+            name="5th"
+            color={fifthColor}
+            animationDuration={colorAnimationDuration}
+          />
+        </View>
       </SafeAreaView>
     );
 };
@@ -62,6 +146,11 @@ const styles = StyleSheet.create({
   infoContainer: {
     width: '100%',
     height: '20%',
+    flexDirection: 'row',
+  },
+  colorContainer: {
+    width: '20%',
+    height: '100%',
   },
 });
 
